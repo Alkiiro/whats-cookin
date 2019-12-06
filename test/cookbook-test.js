0df7698a6cd6b749395daa818d26ed72ad9f8671
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-12-06 15:42:13 -0700

    Add tests for cookbook

--- test/cookbook-test.js
+++ test/cookbook-test.js
@@ -9,15 +9,18 @@ let cookbook;
 describe('User', () => {
   beforeEach(() => {
     cookbook = new Cookbook(recipeData);
+
   });
 
   it('Should have an array of all recipes', () => {
     expect(cookbook.recipes).to.be.an('array');
   });
 
-  it('Should be able to filter through its array with search terms', () => {
-    let searched = cookbook.findRecipe("yolk")
-    console.log(searched);
-    expect(searched.length).to.equal(2);
+  it('Should be able to filter through its array by ingredients', () => {
+    expect(cookbook.findRecipe('yolk').length).to.equal(2);
+  });
+
+  it('Should be able to filter through its array by name', () => {
+    expect(cookbook.findRecipe('Sesame Cookies').length).to.equal(1);
   });
 });
