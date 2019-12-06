0f1f1cae37cb826a3ef2715b903eef4467b06481
Alex Roth <jonesroth7@gmail.com>
2019-12-05 18:31:31 -0700

    Add base constructor for recipe class

---
+++ src/recipe.js
@@ -0,0 +1,15 @@
+class Recipe {
+  constructor(name, id, image, ingredients, instructions, tags) {
+    this.name = name;
+    this.id = id;
+    this.image = image;
+    this.ingredients = ingredients;
+    this.instructions = instructions;
+    this.tags = tags;
+  }
+}
+
+
+if (typeof module !== 'undefined') {
+  module.exports = Recipe;
+}
