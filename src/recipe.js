fcba23024691fbd3af1e7a47b028b255a2abcc75
Alex Roth <jonesroth7@gmail.com>
2019-12-05 21:26:18 -0700

    Add recipeData export/import, add filterFavorites method and testing

--- src/recipe.js
+++ src/recipe.js
@@ -1,12 +1,7 @@
 class Recipe {
-  constructor(name, id, image, ingredients, instructions, tags) {
-    this.name = name;
-    this.id = id;
-    this.image = image;
-    this.ingredients = ingredients;
-    this.instructions = instructions;
-    this.tags = tags;
-  }
+  constructor(recipe) {
+    this.recipe = recipe;
+  };
 }
 
 
--- src/user.js
+++ src/user.js
@@ -10,10 +10,20 @@ class User {
   addToFavorites(recipe) {
     this.favoriteRecipes.push(recipe);
   }
-  
+
   removeFromFavorites(recipe) {
     const i = this.favoriteRecipes.indexOf(recipe);
     this.favoriteRecipes.splice(i, 1)
+  }
+
+  filterFavorites(tag) {
+    return this.favoriteRecipes.filter(recipe => {
+      return recipe.tags.includes(tag);
+    });
+  }
+
+  findFavorites() {
+
   }
 }
 
