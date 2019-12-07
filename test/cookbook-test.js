25f01e67a2aacf551862f0b643bb1bc73486538a
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-12-07 14:38:33 -0700

    Refactor cookbook tests

--- test/cookbook-test.js
+++ test/cookbook-test.js
@@ -9,18 +9,19 @@ let cookbook;
 describe('User', () => {
   beforeEach(() => {
     cookbook = new Cookbook(recipeData);
-
   });
 
   it('Should have an array of all recipes', () => {
     expect(cookbook.recipes).to.be.an('array');
   });
 
-  it('Should be able to filter through its array by ingredients', () => {
-    expect(cookbook.findRecipe('yolk').length).to.equal(2);
-  });
+  describe('findRecipe', () => {
+    it('Should be able to filter through its array by ingredients', () => {
+      expect(cookbook.findRecipe('yolk').length).to.equal(2);
+    });
 
-  it('Should be able to filter through its array by name', () => {
-    expect(cookbook.findRecipe('Sesame Cookies').length).to.equal(1);
+    it('Should be able to filter through its array by name', () => {
+      expect(cookbook.findRecipe('Sesame Cookies').length).to.equal(1);
+    });
   });
-});
+})
