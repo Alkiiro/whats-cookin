4a1a421f7d6c53f46f5e0ea42a190ec5a4785f41
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-12-07 21:46:22 -0700

    Get recipes to go in and out of favorited array on click

--- src/scripts.js
+++ src/scripts.js
@@ -2,9 +2,12 @@
 let cardArea = document.querySelector('.all-cards');
 let favoriteButton = document.querySelector('.favorite');
 let addButton = document.querySelector('.add')
+let cookbook = new Cookbook(recipeData);
 let user;
 
 window.onload = onStartup();
+
+cardArea.addEventListener("click", buttonConditionals);
 
 function onStartup() {
   let userId = (Math.floor(Math.random() * 49) + 1)
@@ -12,30 +15,40 @@ function onStartup() {
     return user.id === Number(userId);
   });
   user = new User(userId, newUser.name, newUser.pantry)
-  populateCards(recipeData);
+  populateCards(cookbook.recipes);
 }
 
-cardArea.addEventListener("click", buttonConditionals);
 
 function favoriteCard(event) {
-  event.target.classList.add('favorite-active');
-  user.favoriteRecipes.push('hello')
-  console.log(user.favoriteRecipes)
-}
-
-function buttonConditionals(event) {
-  if(event.target.classList.contains('favorite')) {
-    favoriteCard(event);
+  let specificRecipe = cookbook.recipes.find(recipe => {
+    // console.log(event.target.id);
+    if (recipe.id === Number(event.target.id)) {
+      return recipe;
+    }
+  })
+  if (!event.target.classList.contains('favorite-active')) {
+    event.target.classList.add('favorite-active');
+    console.log(specificRecipe);
+    user.addToFavorites(specificRecipe);
+  } else if (event.target.classList.contains('favorite-active')) {
+    event.target.classList.remove('favorite-active');
+    user.removeFromFavorites(specificRecipe)
   }
 }
 
+function buttonConditionals(event) {
+  if (event.target.classList.contains('favorite')) {
+    favoriteCard(event);
+    console.log(user.favoriteRecipes);
+  }
+}
 
 function populateCards(recipes) {
   recipes.forEach(recipe => {
     cardArea.insertAdjacentHTML("afterbegin", `<div class='card'>
         <header class='card-header'>
           <img class='add card-button' src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to recipes to cook'>
-          <button class='favorite card-button' alt='Favorite this recipe'></button>
+          <button id='${recipe.id}' class='favorite card-button' alt='Favorite this recipe'></button>
         </header>
           <span class='recipe-name'>${recipe.name}</span>
           <img class='card-picture' src="${recipe.image}" alt="Food from recipe">
