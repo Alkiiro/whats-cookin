906034cb702bc4c4d602610410401f67c9a994e5
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-12-11 12:04:39 -0700

    Fix lint errors

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,9 @@
+// let User = require('../src/user.js');
+// let Cookbook = require('../src/cookbook.js');
+// let recipeData = require('../data/recipes.js');
+// let users = require('../data/users.js');
 
-const favButton = document.querySelector('.view-favorites');
+let favButton = document.querySelector('.view-favorites');
 let homeButton = document.querySelector('.home')
 let cardArea = document.querySelector('.all-cards');
 let cookbook = new Cookbook(recipeData);
@@ -29,28 +33,34 @@ function viewFavorites() {
     favButton.innerHTML = 'You have no favorites!';
     populateCards(cookbook.recipes);
     return
-  } else
-  favButton.innerHTML = 'Your Favorites'
-  cardArea.innerHTML = '';
-  user.favoriteRecipes.forEach(recipe => {
-    cardArea.insertAdjacentHTML("afterbegin", `<div id='${recipe.id}' class='card'>
-        <header id='${recipe.id}' class='card-header'>
-          <label for='add-button' class='hidden'>Click to add recipe</label>
-          <button id='${recipe.id}' class='add-button card-button'>
-            <img id='${recipe.id}' class='add' src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to recipes to cook'>
-          </button>
-          <label for='favorite-button' class='hidden'>Click to favorite recipe</label>
-          <button id='${recipe.id}' class='favorite favorite-active card-button'></button>
-        </header>
-          <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
-          <img id='${recipe.id}' tabindex="0" class='card-picture' src="${recipe.image}" alt="Food from recipe">
-    </div>`)
-  });
-};
+  } else {
+    favButton.innerHTML = 'Your Favorites'
+    cardArea.innerHTML = '';
+    user.favoriteRecipes.forEach(recipe => {
+      cardArea.insertAdjacentHTML("afterbegin", `<div id='${recipe.id}'
+      class='card'>
+      <header id='${recipe.id}' class='card-header'>
+      <label for='add-button' class='hidden'>Click to add recipe</label>
+      <button id='${recipe.id}' class='add-button card-button'>
+      <img id='${recipe.id}' class='add'
+      src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
+      recipes to cook'></button>
+      <label for='favorite-button' class='hidden'>Click to favorite recipe
+      </label>
+      <button id='${recipe.id}' class='favorite favorite-active card-button'>
+      </button></header>
+      <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
+      <img id='${recipe.id}' tabindex="0" class='card-picture'
+      src="${recipe.image}" alt="Food from recipe">
+      </div>`)
+    })
+  }
+}
 
 function greetUser() {
   const userName = document.querySelector('.user-name');
-  userName.innerHTML = user.name.split(' ')[0] + ' ' + user.name.split(' ')[1][0];
+  userName.innerHTML =
+  user.name.split(' ')[0] + ' ' + user.name.split(' ')[1][0];
 }
 
 function favoriteCard(event) {
@@ -72,7 +82,7 @@ function favoriteCard(event) {
 function cardButtonConditionals(event) {
   if (event.target.classList.contains('favorite')) {
     favoriteCard(event);
-   } else if (event.target.classList.contains('card-picture')) {
+  } else if (event.target.classList.contains('card-picture')) {
     displayDirections(event);
   } else if (event.target.classList.contains('home')) {
     populateCards(cookbook.recipes);
@@ -81,7 +91,7 @@ function cardButtonConditionals(event) {
 
 
 function displayDirections(event) {
-   let newRecipeInfo = cookbook.recipes.find(recipe => {
+  let newRecipeInfo = cookbook.recipes.find(recipe => {
     if (recipe.id === Number(event.target.id)) {
       return recipe;
     }
@@ -92,18 +102,23 @@ function displayDirections(event) {
   cardArea.classList.add('all');
   cardArea.innerHTML = `<h3>${recipeObject.name}</h3>
   <p class='all-recipe-info'>
-  <strong>It will cost: </strong><span class='cost recipe-info'>$${costInDollars}</span><br><br>
+  <strong>It will cost: </strong><span class='cost recipe-info'>
+  $${costInDollars}</span><br><br>
   <strong>You will need: </strong><span class='ingredients recipe-info'></span>
-  <strong>Instructions: </strong><ol><span class='instructions recipe-info'></span></ol>
+  <strong>Instructions: </strong><ol><span class='instructions recipe-info'>
+  </span></ol>
   </p>`;
   let ingredientsSpan = document.querySelector('.ingredients');
   let instructionsSpan = document.querySelector('.instructions');
   recipeObject.ingredients.forEach(ingredient => {
-    ingredientsSpan.insertAdjacentHTML("afterbegin", `<ul><li>${ingredient.quantity.amount.toFixed(2)} ${ingredient.quantity.unit} ${ingredient.name}</li></ul>
+    ingredientsSpan.insertAdjacentHTML("afterbegin", `<ul><li>
+    ${ingredient.quantity.amount.toFixed(2)} ${ingredient.quantity.unit}
+    ${ingredient.name}</li></ul>
     `)
   })
   recipeObject.instructions.forEach(instruction => {
-    instructionsSpan.insertAdjacentHTML("beforebegin", `<li>${instruction.instruction}</li>
+    instructionsSpan.insertAdjacentHTML("beforebegin", `<li>
+    ${instruction.instruction}</li>
     `)
   })
 }
@@ -113,17 +128,22 @@ function populateCards(recipes) {
     cardArea.classList.remove('all')
   }
   recipes.forEach(recipe => {
-    cardArea.insertAdjacentHTML("afterbegin", `<div id='${recipe.id}' class='card'>
+    cardArea.insertAdjacentHTML("afterbegin", `<div id='${recipe.id}'
+    class='card'>
         <header id='${recipe.id}' class='card-header'>
           <label for='add-button' class='hidden'>Click to add recipe</label>
           <button id='${recipe.id}' class='add-button card-button'>
-            <img id='${recipe.id} favorite' class='add' src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to recipes to cook'>
+            <img id='${recipe.id} favorite' class='add'
+            src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
+            recipes to cook'>
           </button>
-          <label for='favorite-button' class='hidden'>Click to favorite recipe</label>
+          <label for='favorite-button' class='hidden'>Click to favorite recipe
+          </label>
           <button id='${recipe.id}' class='favorite card-button'></button>
         </header>
           <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
-          <img id='${recipe.id}' tabindex="0" class='card-picture' src="${recipe.image}" alt="click to view recipe for ${recipe.name}">
+          <img id='${recipe.id}' tabindex="0" class='card-picture'
+          src="${recipe.image}" alt="click to view recipe for ${recipe.name}">
     </div>`)
-  });
+  })
 };
